7875d6853c33a67ca05caa0b281751c1e9e33e4d
Pine Wu <octref@gmail.com>
2019-04-17 15:28:48 -0700

    One more fix for insiders download url resolve

--- lib/util.ts
+++ lib/util.ts
@@ -67,7 +67,7 @@ export function insidersDownloadDirToExecutablePath(dir: string) {
 	if (process.platform === 'win32') {
 		return path.resolve(dir, 'Code - Insiders.exe');
 	} else if (process.platform === 'darwin') {
-		return path.resolve(dir, 'Visual Studio Code - Insiders/Contents/MacOS/Electron');
+		return path.resolve(dir, 'Visual Studio Code - Insiders.app/Contents/MacOS/Electron');
 	} else {
 		return path.resolve(dir, 'VSCode-linux-x64/code-insiders');
 	}
