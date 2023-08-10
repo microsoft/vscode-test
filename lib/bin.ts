28f73897d8388e585450f47fb71d125f71692a2d
Remco Haszing <remcohaszing@gmail.com>
2023-08-10 10:57:20 +0200

    Import parseArgs from util

--- lib/bin.ts
+++ lib/bin.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { resolve } from 'path';
-import { parseArgs } from '@pkgjs/parseargs';
+import { parseArgs } from 'util';
 import { runTests } from './runTest';
 
 const options = {
