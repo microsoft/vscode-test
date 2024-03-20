1305af5be4928cb1ca0da7aa7eeccd4647f7478c
yoshinorin <yoshinorin.net@outlook.com>
2024-03-21 01:17:14 +0900

    chore(deps): update `glob` and remove `@types/glob` (#262)
    - the glob 9+ use promise instead of callback
    - the glob 9+ supports typescript, there is no need to install typedefinition dependencies

--- sample/src/test/suite/index.ts
+++ sample/src/test/suite/index.ts
@@ -1,29 +1,29 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import { glob } from 'glob';
 
 export function run(testsRoot: string, cb: (error: any, failures?: number) => void): void {
 	// Create the mocha test
 	const mocha = new Mocha({
-		ui: 'tdd'
+		ui: 'tdd',
 	});
 
-	glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-		if (err) {
+	glob('**/**.test.js', { cwd: testsRoot })
+		.then((files) => {
+			// Add files to the test suite
+			files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+			try {
+				// Run the mocha test
+				mocha.run((failures) => {
+					cb(null, failures);
+				});
+			} catch (err) {
+				console.error(err);
+				cb(err);
+			}
+		})
+		.catch((err) => {
 			return cb(err);
-		}
-
-		// Add files to the test suite
-		files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-		try {
-			// Run the mocha test
-			mocha.run(failures => {
-				cb(null, failures);
-			});
-		} catch (err) {
-			console.error(err);
-			cb(err);
-		}
-	});
+		});
 }
